fb4a0b6bcbcbe1f062aeb0be0142de95ad0fc2b5
micah <micahlynn.santos@gmail.com>
2026-07-06 05:22:43 +0800

    implemented serializable

--- User.java
+++ User.java
@@ -1,13 +1,20 @@
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
     private String username;
     private Library library;
 
-    public User(String usn, Library lib) {
-        username = usn;
-        library = lib;
+    public User(String username) {
+        this.username = username;
+        this.library =  new Library();
     }
 
     public String getUsername() {
         return username;
+    }
+
+    public Library getLibrary() {
+        return library;
     }
 }
