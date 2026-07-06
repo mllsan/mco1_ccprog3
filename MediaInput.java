844bcb19ad12036c5c60675b67aed82bf0c7dd3f
micah <micahlynn.santos@gmail.com>
2026-07-07 01:50:05 +0800

    added error to catch duplicate reviews

--- MediaInput.java
+++ MediaInput.java
@@ -21,40 +21,48 @@ public class MediaInput {
         }
     }
 
-    public MediaEntry createMediaReview(int mediaChoice){
-        int rating = 0, numOfEps = 0, duration = 0;
+    public MediaEntry createMediaReview(int mediaChoice, Library library){
+        int rating = 0, numOfEps = 0, duration = 0, reviewExists = 1;
         String review = "", artist = "";
 
         System.out.print("Enter Title: ");
         String title = input.nextLine();
-        Status status = statusChosen();
 
-        if (mediaChoice == 1){
-            System.out.print("Enter Number of Episodes: ");
-            numOfEps = Integer.parseInt(input.nextLine());
-        } else if (mediaChoice == 2){
-            System.out.print("Enter Duration (minutes): ");
-            duration = Integer.parseInt(input.nextLine());
-        } else if (mediaChoice == 3){
-            System.out.print("Enter Artist Name: ");
-            artist = input.nextLine();
+        if (library.getEntry(title) != null) {
+            System.out.println("Error: An Entry with the Title '" + title + "' Already Exists in the Library.");
+            reviewExists = -1; 
         }
 
-        if (status != Status.PLANNED){
-            System.out.print("Enter Rating (1-5): ");
-            rating = Integer.parseInt(input.nextLine());
+        if (reviewExists!=-1){
+            Status status = statusChosen();
 
-            System.out.print("Enter Review: ");
-            review = input.nextLine();
-        }
+            if (mediaChoice == 1){
+                System.out.print("Enter Number of Episodes: ");
+                numOfEps = Integer.parseInt(input.nextLine());
+            } else if (mediaChoice == 2){
+                System.out.print("Enter Duration (minutes): ");
+                duration = Integer.parseInt(input.nextLine());
+            } else if (mediaChoice == 3){
+                System.out.print("Enter Artist Name: ");
+                artist = input.nextLine();
+            }
 
-        if (mediaChoice == 1){
-            return new Anime(title, status, rating, review, numOfEps);
-        } else if (mediaChoice == 2){
-            return new Movie(title, status, rating, review, duration);
+            if (status != Status.PLANNED){
+                System.out.print("Enter Rating (1-5): ");
+                rating = Integer.parseInt(input.nextLine());
 
-        } else if (mediaChoice == 3){
-            return new Album(title, status, rating, review, artist);
+                System.out.print("Enter Review: ");
+                review = input.nextLine();
+            }
+
+            if (mediaChoice == 1){
+                return new Anime(title, status, rating, review, numOfEps);
+            } else if (mediaChoice == 2){
+                return new Movie(title, status, rating, review, duration);
+
+            } else if (mediaChoice == 3){
+                return new Album(title, status, rating, review, artist);
+            }
         }
 
         return null;
