033bf9617b9861d0ace3be64f277579773daeb0a
jessletran <jessica.letran687@gmail.com>
2026-07-06 23:29:18 +0800

    Edited Menus

--- Main.java
+++ Main.java
@@ -38,28 +38,82 @@ public class Main {
             System.out.println("     MAIN MENU - " + activeAccount.getUsername().toUpperCase());
             System.out.println("===================================");
             System.out.println("[1] View My Library");
-            System.out.println("[2] Add Media Reviews");
-            System.out.println("[3] View Library Summary");
-            System.out.println("[4] Save Progress");
+            System.out.println("[2] Add Media Entry");
+            System.out.println("[3] Edit Existing Entry");
+            System.out.println("[4] View Library Summary");
+            System.out.println("[5] Save Progress");
             System.out.println("[0] Save and Exit");
             System.out.print(">> ");
-            
-            int mainChoice = Integer.parseInt(scanner.nextLine());
+            int mainChoice = InputChecker.getValidInput(scanner,0,5);
             System.out.println("");
 
             switch (mainChoice){
                 case 1:
-                    activeAccount.getLibrary().displayEntries();
+                    System.out.println("View Library By:");
+                    System.out.println("[1] All Entries");
+                    System.out.println("[2] Status");
+                    System.out.println("[3] Media Type");
+                    System.out.print(">> ");
+                    int viewChoice = InputChecker.getValidInput(scanner,1,3);
+
+                    switch (viewChoice) {
+                        case 1:
+                            activeAccount.getLibrary().displayEntries();
+                            break;
+                        case 2:
+                            System.out.println("Choose Status:");
+                            System.out.println("[1] Planned");
+                            System.out.println("[2] In Progress");
+                            System.out.println("[3] Completed");
+                            System.out.print(">> ");
+                            int statusChoice = InputChecker.getValidInput(scanner,1,3);
+
+                            switch (statusChoice){
+                                case 1: 
+                                    activeAccount.getLibrary().displayEntriesByStatus(Status.PLANNED);
+                                    break;
+                                case 2: 
+                                    activeAccount.getLibrary().displayEntriesByStatus(Status.INPROGRESS);
+                                    break;
+                                case 3: 
+                                    activeAccount.getLibrary().displayEntriesByStatus(Status.COMPLETED);
+                                    break;  
+                                default:
+                                    System.out.println("Error: Invalid Option");
+                            }
+                        case 3:
+                            System.out.println("Choose Media Type:");
+                            System.out.println("[1] Anime");
+                            System.out.println("[2] Movie");
+                            System.out.println("[3] Album");
+                            System.out.print(">> ");
+                            int typeChoice = InputChecker.getValidInput(scanner,1,3);
+
+                            switch (typeChoice) {
+                                case 1: 
+                                    activeAccount.getLibrary().displayEntriesByMediaType("Anime");
+                                    break;
+                                case 2:
+                                    activeAccount.getLibrary().displayEntriesByMediaType("Movie");
+                                    break;
+                                case 3:
+                                    activeAccount.getLibrary().displayEntriesByMediaType("Album");
+                                    break;
+                                default:
+                                    System.out.println("Error: Invalid Option");
+                            }
+                        default:
+                            System.out.println("Error: Invalid Option");
+                    }
                     break;
                     
                 case 2:
-                    System.out.println("Choose an Action:");
-                    System.out.println("[1] Add Anime");
-                    System.out.println("[2] Add Movie");
-                    System.out.println("[3] Add Album");
-                    System.out.println("[4] Edit Existing Media");
+                    System.out.println("Choose which Media to add:");
+                    System.out.println("[1] Anime");
+                    System.out.println("[2] Movie");
+                    System.out.println("[3] Album");
                     System.out.print(">> ");
-                    int mediaChoice = Integer.parseInt(scanner.nextLine());
+                    int mediaChoice = InputChecker.getValidInput(scanner,1,3);
                     
                     if (mediaChoice == 1){
                         Anime anime = media.createAnimeReview();
@@ -73,47 +127,30 @@ public class Main {
                         Album album = media.createAlbumReview();
                         activeAccount.getLibrary().addEntry(album);
                     }
-                    else if (mediaChoice == 4){
-                        boolean inEditMenu = true;
-
-                        while (inEditMenu){
-                            System.out.println("\nChoose an Option:");
-                            System.out.println("[1] Edit Anime");
-                            System.out.println("[2] Edit Movie");
-                            System.out.println("[3] Edit Album");
-                            System.out.println("[4] Go Back to Main Menu");
-                            System.out.print(">> ");
-                            int editChoice = Integer.parseInt(scanner.nextLine());
-                            System.out.println("");
-
-                            if (editChoice == 1){
-                                media.editAnimeReview(activeAccount.getLibrary());
-                            } 
-                            else if (editChoice == 2){
-                                media.editMovieReview(activeAccount.getLibrary());
-                            } 
-                            else if (editChoice == 3){
-                                media.editAlbumReview(activeAccount.getLibrary());
-                            } 
-                            else if (editChoice == 4){
-                                System.out.println("Returning to Main Menu...");
-                                inEditMenu = false;
-                            } 
-                            else{
-                                System.out.println("Error: Invalid Option");
-                            }
-                        }
-                    }
                     else{
                         System.out.println("Error: Invalid Option");
                     }
                     break;
                     
                 case 3:
+                    System.out.println("Enter the title of the entry you want to edit:");
+                    System.out.print(">> ");
+                    String title = scanner.nextLine();
+                    
+                    MediaEntry entry = activeAccount.getLibrary().getEntry(title);
+
+                    if (entry == null) {
+                        System.out.println(title + " not found.");
+                    } else {
+                        // need to edit MediaInput first
+                    }
+                    break;
+
+                case 4:
                     activeAccount.getLibrary().displaySummary();
                     break;
                     
-                case 4:
+                case 5:
                     SaveData.saveAccount(activeAccount);
                     break;
                     
