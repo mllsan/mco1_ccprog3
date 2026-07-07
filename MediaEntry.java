a51d5bc9df3237844b8ec1b834e04c0b963cdddf
jessletran <jessica.letran687@gmail.com>
2026-07-07 15:48:21 +0800

    Added restrictions and setTitle

--- MediaEntry.java
+++ MediaEntry.java
@@ -1,4 +1,4 @@
-public class MediaEntry implements java.io.Serializable {
+public abstract class MediaEntry implements java.io.Serializable {
     private String title;
     private Status status;
     private int rating;
@@ -7,14 +7,28 @@ public class MediaEntry implements java.io.Serializable {
     public MediaEntry(String title, Status status, int rating, String review){
         this.title = title;
         this.status = status;
-        this.rating = rating;
-        this.review = review;
+
+        if(status == Status.COMPLETED) {
+            this.rating = rating;
+            this.review = review;
+        } else {
+            this rating = -1;
+            this.review = "";
+        }
     }
 
-    public abstract getMediaType();
+    public abstract String getMediaType();
 
     public String getTitle(){
         return title;
+    }
+
+    public void setTitle(String title) {
+        if (title == null || title.isBlank()) {
+            System.out.println("Title cannot be empty.");
+        } else {
+            this.title = title;
+        }
     }
 
     public Status getStatus(){
@@ -23,6 +37,11 @@ public class MediaEntry implements java.io.Serializable {
 
     public void setStatus(Status status){
         this.status = status;
+
+        if (status != Status.COMPLETED) {
+            rating = -1;
+            review = "";
+        }
     }
 
     public int getRating(){
@@ -30,7 +49,15 @@ public class MediaEntry implements java.io.Serializable {
     }
 
     public void setRating(int rating){
-        this.rating = rating;
+        if (status == Status.COMPLETED) {
+            if (rating < 1 || rating > 10) 
+                System.out.println("Rating must be between 1 and 10");
+            else
+                this.rating = rating;
+        } else {
+            System.out.println("Only completed entries can be rated.");
+            this.rating = -1;
+        }
     }
 
     public String getReview(){
@@ -38,6 +65,11 @@ public class MediaEntry implements java.io.Serializable {
     }
 
     public void setReview(String review){
-        this.review = review;
+        if (status == Status.COMPLETED)
+            this.review = review;
+        else {
+            System.out.println("Only completed entries can be reviewed.");
+            this.review = "";
+        }
     }
 }
